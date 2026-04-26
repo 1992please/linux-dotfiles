e72725aae17caf62a8b992749e8ba37feee6abb2
Nader Hegazy <eng.nadernour@gmail.com>
2026-04-26 10:03:37 +0300

    some changes to nvim configs

---
+++ .config/nvim/lua/config/keybinds.lua
@@ -0,0 +1,4 @@
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
